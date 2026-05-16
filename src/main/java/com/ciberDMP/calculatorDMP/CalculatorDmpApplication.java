3e1637a4a804c9cc135efd1f3c6e7a2e5e948be0
Diego Marful <diego@example.com>
2026-05-16 20:42:09 +0200

    Añadido proyecto Spring Boot con Gradle

---
+++ src/main/java/com/ciberDMP/calculatorDMP/CalculatorDmpApplication.java
@@ -0,0 +1,13 @@
+package com.ciberDMP.calculatorDMP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorDmpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorDmpApplication.class, args);
+	}
+
+}
