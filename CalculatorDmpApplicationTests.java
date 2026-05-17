b5667b9d26878dec732d44fec7a50170cb623db1
Diego Marful <diego@example.com>
2026-05-17 13:02:54 +0200

    Se añaden suma, controlador y test unitario

---
+++ CalculatorDmpApplicationTests.java
@@ -0,0 +1,20 @@
+package com.ciberDMP.calculatorDMP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class CalculatorDmpApplicationTests {
+
+    private Calculator calculator = new Calculator();
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    public void testSum() {
+        assertEquals(5, calculator.sum(2, 3));
+    }
+}
