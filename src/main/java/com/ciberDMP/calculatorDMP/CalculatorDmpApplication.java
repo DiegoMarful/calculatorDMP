e4ec352f448e5dbd0b3684cdd268f7f9fa15088c
Diego Marful <diego@example.com>
2026-05-17 16:51:56 +0200

    Eliminar tabuladores para cumplir con checkstyle

--- src/main/java/com/ciberDMP/calculatorDMP/CalculatorDmpApplication.java
+++ src/main/java/com/ciberDMP/calculatorDMP/CalculatorDmpApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CalculatorDmpApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CalculatorDmpApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatorDmpApplication.class, args);
+    }
 
 }
